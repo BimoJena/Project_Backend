f6ae0947084e0fa58d349fc3d9a0acde4e4123c3
BimoJena <taskbimo@gmail.com>
2025-12-06 13:10:59 +0530

    add first collection data for test

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import dotenv from "dotenv"
 dotenv.config()
 
 import connectDB from "./db/index.js";
+import { Dummy } from "./models/dummy.model.js"; // path correct rakhna
 
-connectDB()
+connectDB().then(async () => {
+  const user = await Dummy.create({ username: "bimochan" });
+  console.log("Dummy data inserted:", user);
+});
